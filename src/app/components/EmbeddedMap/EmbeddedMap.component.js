60de4b1f6ca9d521c7b94fd6821b8bd1d1670872
winniepukki <4bogaa25@solent.ac.uk>
2021-02-09 01:05:44 +0200

    EmbeddedMapComponent fixes

--- src/app/components/EmbeddedMap/EmbeddedMap.component.js
+++ src/app/components/EmbeddedMap/EmbeddedMap.component.js
@@ -1,29 +1,27 @@
-/* eslint-disable */
 import React from 'react';
 import {
-    MAP_SRC,
-    MAP_WIDTH,
-    MAP_HEIGHT
+  MAP_SRC,
+  MAP_WIDTH,
+  MAP_HEIGHT
 } from './EmbeddedMap.config';
 import './EmbeddedMap.style.scss';
-import { withTranslation } from 'react-i18next';
 
 class EmbeddedMapComponent extends React.Component {
   render() {
     return (
-      <div className={'EmbeddedMap-Component'}>
-          <iframe
-              src={MAP_SRC}
-              width={MAP_WIDTH}
-              height={MAP_HEIGHT}
-              frameBorder="0"
-              allowFullScreen=""
-              aria-hidden="false"
-              tabIndex="0"
-          />
+      <div className="EmbeddedMap-Component">
+        <iframe
+          title="EmbeddedMapComponent"
+          src={MAP_SRC}
+          width={MAP_WIDTH}
+          height={MAP_HEIGHT}
+          frameBorder="0"
+          allowFullScreen=""
+          aria-hidden="false"
+        />
       </div>
     );
   }
 }
 
-export default withTranslation()(EmbeddedMapComponent);
+export default EmbeddedMapComponent;
