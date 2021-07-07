b2f966ec97d899de4561de8c4d453c6d18964d7f
winniepukki <4bogaa25@solent.ac.uk>
2021-07-07 11:52:00 +0300

    Shipped sides category with translation

--- src/app/component/ProductList/ProductList.config.js
+++ src/app/component/ProductList/ProductList.config.js
@@ -11,6 +11,7 @@ export const DESSERTS = 'desserts';
 export const BREAKFAST = 'breakfast';
 export const SNACKS = 'snacks';
 export const SALADS = 'salads';
+export const SIDES = 'sides';
 export const SOUPS = 'soups';
 export const SPECIAL = 'special';
 export const KIDS = 'kids';
