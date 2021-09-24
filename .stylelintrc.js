c5d8b0555ce167ae9eb22edd44dc9e6d8cd968fd
winniepukki <4bogaa25@solent.ac.uk>
2021-09-24 11:02:48 +0300

    feature #68 – Supplied Stylelint with adjustments

---
+++ .stylelintrc.js
@@ -0,0 +1,33 @@
+/**
+ * SIA Trinat restaurant project
+ * Copyright © winniepukki. All rights reserved.
+ *
+ * @license MIT
+ */
+module.exports = {
+    'plugins': [
+        'stylelint-scss',
+        'stylelint-order'
+    ],
+    'fix': true,
+    'extends': [
+        'stylelint-config-standard'
+    ],
+    'rules': {
+        'unit-allowed-list': ['ms', 'fr', 'px', 'em', 'rem', '%', 's', 'vmin', 'vmax', 'vh', 'deg'],
+        'at-rule-no-unknown': null,
+        'at-rule-empty-line-before': null,
+        'declaration-empty-line-before': null,
+        'indentation': 4,
+        'number-leading-zero': 'never',
+        'no-descending-specificity': null,
+        'string-quotes': ['single', { 'avoidEscape': true }],
+        'scss/at-rule-no-unknown': true,
+        'scss/selector-no-redundant-nesting-selector': true,
+        'order/order': [
+            'custom-properties',
+            'declarations',
+            'rules'
+        ]
+    }
+}
