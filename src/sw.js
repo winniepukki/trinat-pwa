48496a06357005b330b58b0f104f62bed9f15465
winniepukki <4bogaa25@solent.ac.uk>
2021-11-09 15:50:45 +0000

    feature #92 – SW caching experimental improvements

--- src/sw.js
+++ src/sw.js
@@ -7,38 +7,38 @@
 
 self.addEventListener('activate', () => self.clients.claim());
 
-self.addEventListener('install', async () => {
-    // event.waitUntil(
-    //     caches.open('v1').then((cache) => cache.addAll([
-    //         'index.html',
-    //         'css/main.css',
-    //         'css/vendor.css',
-    //         'css/App.css',
-    //         'css/Footer.css',
-    //         'css/Navigation.css',
-    //         'main.js',
-    //         'vendor.js',
-    //         'App.js',
-    //         'Footer.js',
-    //         'Navigation.js',
-    //         'ScrollTop.js',
-    //         'Router.js'
-    //     ]))
-    // );
+self.addEventListener('install', async (event) => {
+    event.waitUntil(
+        caches.open('v4').then((cache) => cache.addAll([
+            'index.html',
+            'css/main.css',
+            'css/vendor.css',
+            'css/App.css',
+            'css/Footer.css',
+            'css/Navigation.css',
+            'main.js',
+            'vendor.js',
+            'App.js',
+            'Footer.js',
+            'Navigation.js',
+            'ScrollTop.js',
+            'Router.js'
+        ]))
+    );
 });
 
-self.addEventListener('fetch', () => {
+self.addEventListener('fetch', (ev) => {
     // console.log(`Service worker intercepted request for: ${ev.request.url}`);
-    // ev.respondWith(
-    //     caches.match(ev.request)
-    //         .then((res) => {
-    //             if (res) {
-    //                 // console.log('This is in the cache');
-    //                 return res;
-    //             }
-    //
-    //             // console.log('This is NOT in the cache - fetching from web');
-    //             return fetch(ev.request);
-    //         })
-    // );
+    ev.respondWith(
+        caches.match(ev.request)
+            .then((res) => {
+                if (res) {
+                    // console.log('This is in the cache');
+                    return res;
+                }
+
+                // console.log('This is NOT in the cache - fetching from web');
+                return fetch(ev.request);
+            })
+    );
 });
