67376cb72ceaa822ee2d1dcd756bafeb97b1685e
winniepukki <4bogaa25@solent.ac.uk>
2021-11-11 22:13:54 +0000

    feature #92 – IndexedDB store products initial functionality

--- src/app/component/Products/Products.container.js
+++ src/app/component/Products/Products.container.js
@@ -23,10 +23,58 @@ export class ProductsContainer extends React.Component {
     }
 
     async componentDidMount() {
-        const categories = await this.getProductList();
+        const categories = await this.getProductList()
+            .catch(() => this.getProductsFromIndexedDB());
+
         this.setState({
             categories
         });
+
+        this.putProductsToIndexedDB();
+    }
+
+    getProductsFromIndexedDB() {
+        const request = window.indexedDB.open('products', 1);
+        request.onsuccess = (event) => {
+            const db = event.target.result;
+            const transaction = db.transaction('products');
+            const objectStore = transaction.objectStore('products');
+            const request = objectStore.getAll();
+
+            request.onsuccess = () => {
+                const { result: products = [] } = request;
+
+                return products;
+            };
+        };
+    }
+
+    putProductsToIndexedDB() {
+        // eslint-disable-next-line no-unused-vars
+        let db;
+        const request = window.indexedDB.open('products', 1);
+
+        const { categories } = this.state;
+
+        request.onsuccess = (e) => {
+            db = e.target.result;
+        };
+
+        request.onupgradeneeded = (e) => {
+            const db = e.target.result;
+
+            if (db.version >= 2) {
+                db.deleteObjectStore('products');
+            }
+
+            const objectStore = db.createObjectStore('products', {
+                keyPath: 'key'
+            });
+
+            for (let i = 0; i < categories.length; i++) {
+                objectStore.add(categories[i]);
+            }
+        };
     }
 
     containerProps() {
