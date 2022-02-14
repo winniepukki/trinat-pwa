4ae1aba4ad7622c40de17358fffa4261d3c5b1f4
winniepukki <4bogaa25@solent.ac.uk>
2022-02-14 03:32:24 +0200

    feature #158 – Updated the SW version

--- src/sw.js
+++ src/sw.js
@@ -5,7 +5,7 @@
 * @license MIT
 */
 
-const cacheVersion = 'v8';
+const cacheVersion = 'v11';
 
 self.addEventListener('activate', () => {
     caches.keys().then((keyList) => Promise.all(keyList.map((key) => {
