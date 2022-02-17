21fbc76289eafa157446406e140e0a09f562be6a
winniepukki <4bogaa25@solent.ac.uk>
2022-02-17 02:32:08 +0200

    feature #154 – Updated the Privacy Policy dates

--- src/sw.js
+++ src/sw.js
@@ -5,7 +5,7 @@
 * @license MIT
 */
 
-const cacheVersion = 'v11';
+const cacheVersion = 'v12';
 
 self.addEventListener('activate', () => {
     caches.keys().then((keyList) => Promise.all(keyList.map((key) => {
