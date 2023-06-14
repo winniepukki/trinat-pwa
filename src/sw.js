6ee1e936f1537643d304f8459581464a90f75549
winniepukki <a.bogackins@gmail.com>
2023-06-14 19:55:53 +0300

    feature/#195 – Add initial LogRocket config

--- src/sw.js
+++ src/sw.js
@@ -5,7 +5,7 @@
 * @license MIT
 */
 
-const cacheVersion = 'v20';
+const cacheVersion = 'v21';
 
 self.addEventListener('activate', () => {
     caches.keys().then((keyList) => Promise.all(keyList.map((key) => {
